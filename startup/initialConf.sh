57320dcbba6d62463f4d43246747d984aa603d37
carlitomm <cmiguelezmachado@gmail.com>
2021-07-23 19:37:13 -0600

    added web_soquet_server

---
+++ startup/initialConf.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+cp ~/catkin_ws/src/domotics/startup/ /etc/systemd/system/uvbot-bringup.service
---
+++ startup/startup.sh
@@ -0,0 +1,5 @@
+#! usr/bin/env bash
+
+source "opt/ros/melodic/setup.bash"
+source "/home/carlos/catkin_ws/devel/setup.bash"
+roslaunch domotics domotics.launch 
